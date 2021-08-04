e4a3e7c6de0209b8d5a4276c30f1f695f92684c5
MonroLex <monroylex2@outlook.com>
2021-08-04 11:51:23 -0500

    Comienzo proyecto Reproductor de musica

---
+++ CSMusicPlayer/MusicPlayer.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace CSMusicPlayer
+{
+    partial class MusicPlayer
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MusicPlayer));
+            this.songsList = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // songsList
+            // 
+            this.songsList.FormattingEnabled = true;
+            this.songsList.Location = new System.Drawing.Point(591, 18);
+            this.songsList.Name = "songsList";
+            this.songsList.Size = new System.Drawing.Size(184, 329);
+            this.songsList.TabIndex = 0;
+            this.songsList.DoubleClick += new System.EventHandler(this.songsList_DoubleClick);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(591, 362);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(83, 39);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(693, 362);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(82, 39);
+            this.btnRemove.TabIndex = 2;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(12, 18);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(555, 383);
+            this.axWindowsMediaPlayer1.TabIndex = 3;
+            // 
+            // MusicPlayer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 410);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.songsList);
+            this.Name = "MusicPlayer";
+            this.Text = "Music Player";
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox songsList;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemove;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+    }
+}
+
---
+++ CSMusicPlayer/MusicPlayer.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSMusicPlayer
+{
+    public partial class MusicPlayer : Form
+    {
+        //se crean dos variables locales fuera del metodo btnAdd_Click para contener tanto la ruta como los nombres de los archivos de mucica
+        List<string> _sonNames; //nombres de los archivos
+        List<string> _sonPaths; //lista con la ruta de los archivos
+
+        public MusicPlayer()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            //la clase OpenFileDialog es la que muestra la ventana para seleccionar el archivo
+            OpenFileDialog dialog = new OpenFileDialog();
+
+            //la funcion Multiselect permiter seleccinar mas dun archivo 
+            dialog.Multiselect = true;
+
+            //despues de instanciar la clase OpenFileDialog utilizamos el metodo .ShowDialog() el cual retorna un DialogResult.OK si el usuario selecciona un archivo
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                //dialog.SafeFileNames esta propiedad nos permite traer el nombre del archivo y la ruta donde se encuentra 
+                //dialog.SafeFileName esta propiedad solo nos trae el nombre del archivo
+                //con el metodo ToList() convertimos un array a List
+                //_sonNames = dialog.SafeFileNames.ToList();
+                //_sonPaths = dialog.FileNames.ToList();
+
+                AddSongsToList(dialog.SafeFileNames.ToList() ,dialog.FileNames.ToList());
+            }
+        }
+
+        //secrea un metodo que agregue los archivos a la lista de reproduccion
+        //recibe como parametros las listas de names y ruta de archivos 
+        private void AddSongsToList(List<string> names , List<string> paths)
+        {
+            //se crea un if para inicializar la lista _sonNames que esta sin inicializar
+            if (_sonNames == null)
+                _sonNames = new List<string>();
+
+            if (_sonPaths == null)
+                _sonPaths = new List<string>();
+
+            foreach (var item in names)
+            {
+                //si la cancion existe o no 
+                if (!ExistsOnList(item))
+                {
+                    _sonNames.Add(item);
+                   _sonPaths.Add(GetPath(item,paths));
+                }
+            }
+
+            //agregar las canciones al ListsBox 
+            songsList.DataSource = _sonNames;
+
+            RefreshList();
+        }
+
+
+        //se crea el metodo ExistsOnList para saber si existe o no el nombre de la cancion
+        private bool ExistsOnList(string song)
+        {
+            bool exist = false;
+
+            foreach (var item in _sonNames)
+            {
+                if (item == song)
+                
+                    exist = true;
+            }
+
+            return exist;
+        }
+
+        //se crea un nuevo metodo actualiza y contiene las rutas de las canciones
+        private string GetPath(string FileName , List<string> songsPath = null)
+        {
+            string actualPath = string.Empty;
+
+            if (songsPath == null)
+                songsPath = _sonPaths;
+
+            foreach (var path in songsPath)
+            {
+                if (path.Contains(FileName))
+                    actualPath = path;
+            }
+            return actualPath;
+        }
+
+        private void songsList_DoubleClick(object sender, EventArgs e)
+        {
+            string a = songsList.Text;
+            axWindowsMediaPlayer1.URL = GetPath(songsList.Text);
+        }
+
+        //se crea el evnto click para el boton remover cancion
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            if (_sonNames != null)
+                _sonNames.Remove(songsList.Text);
+
+            if (_sonPaths != null)
+                _sonPaths.Remove(GetPath(songsList.Text));
+
+            RefreshList();
+        }
+
+
+        //se crea un metodo para refrescar la lista de reproduccion
+        private void RefreshList()
+        {
+            songsList.DataSource = null;
+            songsList.DataSource = _sonNames;
+        }
+    }
+}
